741c5c5c649c4412f1469eca6894cf4105931874
Chiara De Guglielmo <kiradg01@gmail.com>
2024-10-29 09:28:58 +0100

    fix

--- Calculators.cpp
+++ Calculators.cpp
@@ -1,11 +1,25 @@
 #include "Calculators.hpp"
+#include <cmath>
 
+
+double degreeToRadiant(double gradi) {
+    return gradi * (M_PI / 180.0); 
+}
 
 Type::Path FlatEarthPathCalculator::eval() const {
     Type::Path out;
     out.reserve(_path.size()+1);
 
     out.emplace_back(_data.p0);
+
+    Type::Point  p_current = _data.p0;
+
+
+    for (int i = 1; i < _path.size(); ++i) {
+        Type::Point  p_next  = { p_current[0] + (_data.d*_path[i].first)*cos(degreeToRadiant(_data.th + _path[i].second)) ,  p_current[1] + (_data.d*_path[i].first)*sin(degreeToRadiant(_data.th + _path[i].second)) }; 
+        out.emplace_back(p_next);  
+        p_current = p_next;
+    }
 
     return out;
 }
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # academy_project
 Final progect of DEV - TXT academy
+
+
+# Requisiti?
+- dist massima = 2000 km = 0.5 NM
+- nomi dei punti ordine alfabetico
