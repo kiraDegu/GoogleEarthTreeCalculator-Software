855efafcd18972d993ff3eb30cb2c974f212cfa4
nicholas-mallamaci <92762832+nicholas-mallamaci@users.noreply.github.com>
2024-10-29 14:04:01 +0100

    .

--- devacademy/mainwindow.h
+++ devacademy/mainwindow.h
@@ -2,12 +2,25 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
+#include <QMessageBox>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
 }
 QT_END_NAMESPACE
+
+class HoverLabel : public QLabel {
+    Q_OBJECT
+
+public:
+    HoverLabel(const QString &text, QWidget *parent = nullptr);
+
+protected:
+    void enterEvent(QEnterEvent *event) override;
+    void leaveEvent(QEvent *event) override;
+};
 
 class MainWindow : public QMainWindow
 {
@@ -23,6 +36,7 @@ private slots:
 
 private:
     Ui::MainWindow *ui;
+    HoverLabel *label;
     void UserInput();
     void displayResults();
 };
